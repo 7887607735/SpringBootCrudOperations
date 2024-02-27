efa1624cfc59664252984f1680f7db52c6663c40
Wanve Sonali <92150661+7887607735@users.noreply.github.com>
2024-02-27 12:06:13 +0530

    Add files via upload

---
+++ SpringBootCruDwithDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.exampleOfSpringBootCRUDwithDatabase.SpringBootCRUDwithDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCruDwithDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCruDwithDatabaseApplication.class, args);
+		System.err.println("I am Storing permanent data");
+	}
+
+}
